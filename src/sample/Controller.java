3ea1eda5f8fdcda3d4972edc6dfe32459403ba00
davemustaine <ksenofontov97@mail.ru>
2019-03-20 21:19:48 +0700

    Внешний вид почти готов

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
